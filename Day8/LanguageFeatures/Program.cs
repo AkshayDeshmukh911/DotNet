20dbc897a1ef43b96ee3181f712643efd5929a50
Akshay Deshmukh <65339947+AkshayDeshmukh911@users.noreply.github.com>
2020-12-08 04:58:04 -0800

    Anonymous and Lambda function

---
+++ Day8/LanguageFeatures/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+//Implicit Variables
+namespace LanguageFeatures
+{
+    class Program
+    {
+        static void Main()
+        {
+            int i;
+            var i2 = 100;  //implicit variable
+            var i3 =(short) 100;  //implicit variable
+            //used only for local variables
+            //cant be used for class level vars,fn params and return types
+            //i2 = 200;
+            Console.WriteLine(i2.GetType().ToString());
+            Console.ReadLine();
+
+           
+        }
+    }
+}
