44522434bcc31289143fd374c7fa7164fa32ab41
Akshay Deshmukh <65339947+AkshayDeshmukh911@users.noreply.github.com>
2020-12-10 02:48:07 -0800

    Add files via upload

---
+++ Day5/CDACBatches/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CDACBatches
+{
+    class Program
+    {
+        static void Main()
+        {
+            int b;
+            b = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Enter the NO.of batches in cdac : " + b);
+            Console.WriteLine();
+            Console.WriteLine();
+            int[][] arr = new int[b][];
+
+            for (int i = 0; i < b; i++)
+            {
+                Console.WriteLine($"Enter the No of students in batch " + i + " : ");
+                int p = Convert.ToInt32(Console.ReadLine());
+                arr[i] = new int[p];
+                for (int j = 0; j < p; j++)
+                {
+                    Console.WriteLine($"Marks obtained to student no " + j);
+                      arr[i][j] = Convert.ToInt32(Console.ReadLine());
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0; j < arr[i].Length; j++)
+                {
+                    Console.WriteLine($"Batch {i+1} Student {j+1} marks are : "+arr[i][j]);
+                }
+                
+            }
+        }
+    }
+}
---
+++ Day5/StructCode/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StructCode
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+
+        public struct Student
+        {
+            private string name;
+            private int rollNo;
+            private decimal marks;
+
+            public string Name { get; set; }
+            public int RollNo { get; set; }
+            public decimal Marks { get; set; }
+
+            public Student(string name, int rollNo, decimal marks)
+            {
+                this.Name = name;
+                this.RollNo = rollNo;
+                this.Marks = marks;
+            }
+        }
+    }
+}
