bba2a0adb32e8913e6b65c9037f8952e952bc5fb
Akshay Deshmukh <65339947+AkshayDeshmukh911@users.noreply.github.com>
2020-12-10 02:41:32 -0800

    Add files via upload

---
+++ Day6/DelegateClass/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateClass
+{
+    //step 1 : create a delegate class having the same signature as the function to call
+
+    public delegate void Del1();
+    public delegate int DelAdd(int i, int j);
+
+    class Program
+    {
+        static void Main1()
+        {
+            Del1 objDel;
+            //Display();
+            //step 2 : create a delegate object passing the function name as a parameter
+             objDel = new Del1(Display);
+
+            //step 3 : call the function via the delegate object
+            objDel();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            objDel += new Del1(flow);
+            objDel();
+            Console.WriteLine();
+            Console.WriteLine();
+
+            objDel += new Del1(show);
+            objDel();
+
+           // objDel -= new Del1(show);
+           // objDel();
+            Console.ReadLine();
+        }
+
+        static void Main2()
+        {
+            Del1 objDel;
+            objDel = Display;
+            objDel();
+            objDel += flow;
+            objDel();
+           // objDel += show();
+        }
+
+        static void Main3()
+        {
+            Del1 objDel = (Del1)Delegate.Combine(new Del1(Display),new Del1(flow), new Del1(show));
+            objDel();
+            Console.ReadLine();
+        }
+
+        static void Main4()
+        {
+            Del1 objDel;
+             objDel = Display;
+            objDel();
+            objDel = flow;
+            objDel();
+            objDel = show;
+            objDel();
+            DelAdd objDelAdd = Add;
+            objDelAdd(10,20);
+            Console.WriteLine(objDelAdd(10, 20));
+        }
+
+
+        static void Display()
+        {
+            Console.WriteLine("disp1");
+        }
+        static void flow()
+        {
+            Console.WriteLine("disp2");
+        }
+        static void show()
+        {
+            Console.WriteLine("disp3");
+        }
+        static int Add(int i, int j)
+        {
+            return i + j;
+        }
+       
+    }
+}
---
+++ Day6/HashTableAndSortedList/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace HashTableAndSortedList
+{
+    class Program
+    {
+        //using System.Collection
+        static void Main1()
+        {
+            SortedList objDictionary = new SortedList();
+            objDictionary.Add(1, "abc");
+            objDictionary.Add(2, "def");
+            objDictionary.Add(3, "ghi");
+
+            objDictionary[3] = "new value";
+            objDictionary[4] = "aaaaaaa";
+
+           // objDictionary.Remove(2);
+
+            Console.WriteLine();
+            foreach(DictionaryEntry de in objDictionary)
+            {
+                Console.WriteLine(de.Key);
+                Console.WriteLine(de.Value);
+
+            }
+        }
+
+        //using System.Collections.Generics
+        static void Main()
+        {
+            SortedList<int, string> objDictionary = new SortedList<int, string>();
+            objDictionary.Add(1, "abc");
+            objDictionary.Add(2, "def");
+            objDictionary.Add(3, "ghi");
+
+            objDictionary[3] = "new value";
+            objDictionary[4] = "aaaaaaa";
+
+            // objDictionary.Remove(2);
+
+            Console.WriteLine();
+            foreach (KeyValuePair<int, string> de in objDictionary)
+            {
+                Console.WriteLine(de.Key);
+                Console.WriteLine(de.Value);
+            }
+         }
+    }
+}
