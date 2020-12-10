d6ba5c25240b54b10f16a57971a3f2b23911fb62
Akshay Deshmukh <65339947+AkshayDeshmukh911@users.noreply.github.com>
2020-12-10 02:55:48 -0800

    Add files via upload

---
+++ Day1/BasicClassConcepts/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//using System.Data;
+
+namespace BasicClassConcepts
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Class2
+            //BasicClassConcepts.Class2
+            //n2.Class2
+            //System.Data.DataSet
+            //n2.n3.Class4
+            //System.Console.WriteLine("Hello World");
+            //System.Console.ReadLine();
+            // System.Console.WriteLine("Hello Akshay Welcome in .Net world");
+            //System.Data.DataSet ds = new System.Data.DataSet();
+
+            Class1 o;
+            o = new Class1();
+            o.Display();
+            o.Display("Akshay");
+            Console.ReadLine();
+              
+        }
+    }
+    public class Class1
+    {
+        public void Display()
+        {
+            System.Console.WriteLine("Hello Akshay Welcome in .Net world");
+        }
+
+        //Function overloading
+        public void Display(string s)
+        {
+            Console.WriteLine("Hello " +
+                "" + s);
+        }
+    }
+    class Class2
+    { }
+}
+namespace n2
+{
+    class C3
+    {
+
+    }
+    class Class2
+    {
+
+    }
+    namespace n3
+    {
+        class Class4
+        {
+
+        }
+    }
+}
